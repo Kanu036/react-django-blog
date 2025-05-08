efead0cd00a3c59821814c24360afd30e333076b
kanu036 <john.doe@gmail.com>
2025-05-08 13:48:51 -0500

    Update landing page & background color & homepage text

--- src/hocs/Layout.js
+++ src/hocs/Layout.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 
 const Layout = (props) => {
-    return (
-        <div>
-            <Navbar />
-            {props.children}
-        
-        </div>
+  return (
+    <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
+      <Navbar />
 
-    );
-    
+      <main className="flex-grow-1">
+        {props.children}
+      </main>
+
+      <footer className="text-muted text-center py-3">
+        <p>Created with ❤️ to inspire and share knowledge.</p>
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
